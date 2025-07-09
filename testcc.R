65323bd502561ab73159d83a4488e58458d35186
CassieCai0922 <cassie.cai@sanofi.com>
2025-07-09 11:01:28 +0200

    Second commint

--- testcc.R
+++ testcc.R
@@ -1,9 +1,10 @@
 ###### training for R advance
 
-set.seed(987)
+set.seed(789)
 data <- data.frame(
   subjid=1:10,
   sex=sample(c("Male","Female"),10,replace=TRUE),
   age=sample(18:60,10,replace = TRUE),
-  weight=sample(50:80,10,replace=FALSE)
+  weight=sample(50:80,10,replace=FALSE),
+  height=sample(158:177,10,replace=FALSE)
 )
